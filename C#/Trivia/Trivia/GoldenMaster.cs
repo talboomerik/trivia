67628dfbb265e2d921e8b786379717976e1bbf6f
Jeff Foster <jeff.foster@acm.org>
2013-08-18 20:00:29 +0100

    template golden master

---
+++ C#/Trivia/Trivia/GoldenMaster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Trivia
+{
+    [TestFixture]
+    class GoldenMaster
+    {
+        private const string c_Seed = "SOME_SEED";
+
+        private const string c_ExpectedOutput = "";
+
+        [TestCase(c_Seed, Result = c_ExpectedOutput)]
+        public string CharacterizationTest(string seedArgument)
+        {
+            using (var output = new StringWriter()) 
+            {
+                Console.SetOut(output);
+                GameRunner.Main(new string[]{c_Seed});
+                return output.ToString();
+            }
+        }
+    }
+}
