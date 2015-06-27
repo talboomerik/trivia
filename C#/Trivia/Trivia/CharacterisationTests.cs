b4822940ea2e87d044e304adbe0bc80cec64dadc
prGmtc <jo.vaneyck@gmail.com>
2015-06-27 11:23:07 +0200

    Added a golden master

---
+++ C#/Trivia/Trivia/CharacterisationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace Trivia
+{
+    [TestFixture]
+    public class CharacterisationTests
+    {
+        [Test]
+        [UseReporter(typeof (DiffReporter))]
+        public void GoldenMaster()
+        {
+            var sb = new StringBuilder();
+            var capturedOutput = new StringWriter(sb);
+            Console.SetOut(capturedOutput);
+            GameRunner.Main(new []{"ANY STRING HERE"});
+
+            Approvals.Verify(sb.ToString());
+        }
+    }
+}
