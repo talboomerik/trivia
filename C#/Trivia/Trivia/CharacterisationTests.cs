d8d3a9ddd9515b9ad4a5d39f2a02731f1f163353
prGmtc <jo.vaneyck@gmail.com>
2015-06-27 11:48:05 +0200

    added some golden master tests

--- C#/Trivia/Trivia/CharacterisationTests.cs
+++ C#/Trivia/Trivia/CharacterisationTests.cs
@@ -23,5 +23,17 @@ namespace Trivia
 
             Approvals.Verify(sb.ToString());
         }
+
+        [Test]
+        [UseReporter(typeof (DiffReporter))]
+        public void GoldenMaster2()
+        {
+            var sb = new StringBuilder();
+            var capturedOutput = new StringWriter(sb);
+            Console.SetOut(capturedOutput);
+            GameRunner.Main(new []{"any other plain old string111!!!&"});
+
+            Approvals.Verify(sb.ToString());
+        }
     }
 }
