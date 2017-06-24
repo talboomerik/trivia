284f5e53ae2cc9e1f32835fb555ecca1d8525fcb
koen.metsu <koen.metsu@gmail.com>
2017-06-24 17:13:39 +0200

    Remove redundant parentheses

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -26,8 +26,8 @@ namespace UglyTrivia
             for (int i = 0; i < 50; i++)
             {
                 popQuestions.AddLast("Pop Question " + i);
-                scienceQuestions.AddLast(("Science Question " + i));
-                sportsQuestions.AddLast(("Sports Question " + i));
+                scienceQuestions.AddLast("Science Question " + i);
+                sportsQuestions.AddLast("Sports Question " + i);
                 rockQuestions.AddLast(createRockQuestion(i));
             }
         }
