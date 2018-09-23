c7627741a4545260346137718ee311eb8069e103
koenmetsu <github@kometsoft.be>
2018-09-23 20:07:18 +0200

    Add Golden Master

--- C#/netcore2.1/Trivia/Trivia.Tests/UnitTest1.cs
+++ C#/netcore2.1/Trivia/Trivia.Tests/UnitTest1.cs
@@ -1,14 +1,27 @@
-using System;
-using Xunit;
-
 namespace Trivia.Tests
 {
+    using System.IO;
+    using System.Text;
+    using Xunit;
+
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            Assert.True(true);
+            var goldenMaster = File.ReadAllText("golden_master.txt");
+
+            Assert.True(goldenMaster.Length > 0);
+
+            var stringBuilder = new StringBuilder();
+            using(var writer = new StringWriter(stringBuilder)){
+                System.Console.SetOut(writer);
+                
+                GameRunner.Main(new []{"12"});
+            }
+
+            Assert.True(stringBuilder.ToString().Length > 0);
+            Assert.Equal(goldenMaster, stringBuilder.ToString());
         }
     }
 }
