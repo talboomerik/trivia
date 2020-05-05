5cf3f4aae0df2eefa30e6379323cb7069cc0943d
Harrie Vanhuysse <harrie.vanhuysse@barco.com>
2020-05-05 11:53:43 +0200

    Added fixed seed

--- C++/GameRunner.cpp
+++ C++/GameRunner.cpp
@@ -1,15 +1,25 @@
 ﻿#include <stdlib.h>
+#include <iostream>
+#include <time.h>
 #include "Game.h"
 
 static bool notAWinner;
 
-int main()
+int main(int argc, char* argv[])
 {
+  auto seed = time(NULL);
+  if (argc == 2)
+  {
+    seed = atoi(argv[1]); 
+  }
+
 	Game aGame;
 
 	aGame.add("Chet");
 	aGame.add("Pat");
 	aGame.add("Sue");
+
+  srand(seed);
 
 	do
 	{
